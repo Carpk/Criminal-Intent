e4e4b1b7cd65ed062a5d8dca0f9f97aaec653fec
brechen <zappeln@gmail.com>
2016-08-13 23:17:37 -0500

    create textViews and checkBox for fragment_crime.xml

--- app/src/main/java/net/shawnklein/android/criminalintent/Crime.java
+++ app/src/main/java/net/shawnklein/android/criminalintent/Crime.java
@@ -1,13 +1,33 @@
 package net.shawnklein.android.criminalintent;
 
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
 
     public UUID getId() {
+
         return mId;
     }
 
