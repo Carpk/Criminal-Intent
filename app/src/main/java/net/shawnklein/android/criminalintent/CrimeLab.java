6fb018cf53a14f541b4517e64abf48e9fecc2ae8
brechen <zappeln@gmail.com>
2016-08-19 21:28:09 -0500

    created CrimeLab singleton class to hold array of Crime objects

---
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeLab.java
@@ -0,0 +1,35 @@
+package net.shawnklein.android.criminalintent;
+
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class CrimeLab {
+    private ArrayList<Crime> mCrimes;
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes) {
+            if (c.getId().equals(id))
+                return c;
+        }
+        return null;
+    }
+}
