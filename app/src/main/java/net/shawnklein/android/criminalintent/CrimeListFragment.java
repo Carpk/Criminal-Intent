c4654ec2c0f10b8a0865c3ec90e6a0ee08a1c098
brechen <zappeln@gmail.com>
2016-08-20 20:35:27 -0500

    add loop too load dummy data, create new class for CrimeListFragment

---
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package net.shawnklein.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class CrimeListFragment extends ListFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.crimes_title);
+    }
+}
