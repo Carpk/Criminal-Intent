028c2c3dda613411a7637709f6a2c500de5ce208
brechen <zappeln@gmail.com>
2016-08-23 20:48:04 -0500

    create new CrimeListActivity class, CrimeActivity gets used by abstract class

---
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package net.shawnklein.android.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
