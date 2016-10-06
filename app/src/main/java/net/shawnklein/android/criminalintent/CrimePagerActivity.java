d95da8b5cf270e7fed13096003e29a2b6187d2ee
brechen <zappeln@gmail.com>
2016-10-05 22:40:45 -0500

    create stand alone ID resource

--- app/src/main/java/net/shawnklein/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimePagerActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
-
 public class CrimePagerActivity extends FragmentActivity {
     private ViewPager mViewPager;
 
