bf19d4af627ef6ceefbcafb75a9c9d8697dff88e
brechen <zappeln@gmail.com>
2016-10-30 15:27:29 -0500

    app will toggle subtitles
    create new menu xml file for os v11+

--- app/src/main/java/net/shawnklein/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package net.shawnklein.android.criminalintent;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -79,6 +80,7 @@ public class CrimeListFragment extends ListFragment {
         inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
+    @TargetApi(11)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -88,6 +90,15 @@ public class CrimeListFragment extends ListFragment {
                 Intent i = new Intent(getActivity(), CrimePagerActivity.class);
                 i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
                 startActivityForResult(i, 0);
+                return true;
+            case R.id.menu_item_show_subtitle:
+                if (getActivity().getActionBar().getSubtitle() == null) {
+                    getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                    item.setTitle(R.string.hide_subtitle);
+                } else{
+                    getActivity().getActionBar().setSubtitle(null);
+                    item.setTitle(R.string.show_subtitle);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
