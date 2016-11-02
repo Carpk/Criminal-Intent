fe090530cf684cc4dd7e37d794152d7186e0cc6b
brechen <zappeln@gmail.com>
2016-11-02 18:47:16 -0500

    create camera fragment class

---
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeCameraFragment.java
@@ -0,0 +1,33 @@
+package net.shawnklein.android.criminalintent;
+
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class CrimeCameraFragment extends Fragment {
+    private static final String TAG = "CrimeCameraFragment";
+
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parnet, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_camera, parnet, false);
+
+        Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+
+        return v;
+    }
+}
