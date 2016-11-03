7f22e53742c94ddac210ca99de4262296fee3498
brechen <zappeln@gmail.com>
2016-11-03 17:26:28 -0500

    create callback methods for surfaceHolder

--- app/src/main/java/net/shawnklein/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/net/shawnklein/android/criminalintent/CrimeCameraFragment.java
@@ -4,11 +4,16 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
 
 
 public class CrimeCameraFragment extends Fragment {
@@ -18,6 +23,7 @@ public class CrimeCameraFragment extends Fragment {
     private SurfaceView mSurfaceView;
 
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, ViewGroup parnet, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, parnet, false);
 
@@ -28,11 +34,50 @@ public class CrimeCameraFragment extends Fragment {
             }
         });
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception)
+                }
+            }
+
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                if (mCamera !=  null) {
+                    mCamera.stopPreview();
+                }
+            }
+
+            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+                if (mCamera == null) return;
+
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = null;
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+
+            }
+        });
 
         return v;
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public void onResume() {
         super.onResume();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
