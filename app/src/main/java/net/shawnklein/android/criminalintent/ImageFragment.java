2b8299bed04018d64afc817fff2311b1c4393463
brechen <zappeln@gmail.com>
2016-11-10 21:46:11 -0600

    create ImageFragment class

---
+++ app/src/main/java/net/shawnklein/android/criminalintent/ImageFragment.java
@@ -0,0 +1,20 @@
+package net.shawnklein.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+
+public class ImageFragment extends DialogFragment {
+    public static final String EXTRA_IMAGE_PATH = "net.shawnklein.android.criminalintent.image_path";
+
+    public static ImageFragment newInstance(String imagePath) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_IMAGE_PATH, imagePath);
+
+        ImageFragment fragment = new ImageFragment();
+        fragment.setArguments(args);
+        fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+
+        return fragment;
+    }
+}
